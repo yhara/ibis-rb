75177c7ffc52438aebdfc418502ecb35ab963b11
Yutaka HARA <yutaka.hara+github@gmail.com>
2014-05-10 14:03:55 +0900

    refactor test

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,35 +2,54 @@ require_relative 'spec_helper'
 
 describe Ibis::Parser do
   before do
-    @parser = Ibis::Parser.new
+    parser = Ibis::Parser.new
+    @parse = ->(src){
+      parser.parse(src)
+    }
   end
 
-  context "Literal" do
+  context "Constant" do
     it "int" do
-      expect(@parser.parse("99")).to eq([:Const, 99])
+      expect(@parse["99"]).to eq([:Const, 99])
     end
 
     it "string" do
-      expect(@parser.parse('"abc"')).to eq([:Const, "abc"])
+      expect(@parse['"abc"']).to eq([:Const, "abc"])
     end
 
     it "true" do
-      expect(@parser.parse("true")).to eq([:Const, true])
+      expect(@parse["true"]).to eq([:Const, true])
     end
 
     it "false" do
-      expect(@parser.parse("false")).to eq([:Const, false])
+      expect(@parse["false"]).to eq([:Const, false])
     end
+
+    # tuple
+    # unit
   end
 
   context "Expression" do
     it "~-" do
-      expect(@parser.parse("-9")).to eq([:App, "(~-)", [:Const, 9]])
+      expect(@parse["-9"]).to eq([:App, "(~-)", [:Const, 9]])
     end
 
     it "varref" do
-      expect(@parser.parse("foo")).to eq([:Var, "foo"])
+      expect(@parse["foo"]).to eq([:Var, "foo"])
     end
+
+    # let
+    # let-tuple
+    # let-rec
+    # if
+    # app
+    # binary expr
+    # var def
+    # type expr
+    # paren
+    # case
+    # multiple exprs
+    # compexpr#
   end
 
 end
